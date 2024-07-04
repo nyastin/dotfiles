824583405fd8ee767a7633fa03f197a0365a4de6
nyastin <jstnvlnca@gmail.com>
2024-07-04 11:50:41 +0800

    Feat: New keymap bindings for telescope

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,10 +1,22 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
---
 
-local keymap = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true, desc = "" }
 
+-- Scroll down
+opts.desc = "Scroll down"
 keymap("n", "<C-d>", "<C-d>zz", opts)
+
+-- Update opts for the next keymap
+opts.desc = "Scroll up"
 keymap("n", "<C-u>", "<C-u>zz", opts)
+
+-- Telescope
+local builtin = require("telescope.builtin")
+
+opts.desc = "Live Grep"
+keymap("n", "<C-f>", builtin.live_grep, opts)
+opts.desc = "Find Files"
+keymap("n", "<C-p>", builtin.find_files, opts)
